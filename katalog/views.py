02c8b87562dfee7b40863b7adcc076a3f8258832
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2022-09-11 21:36:02 +0700

    progress 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# Create your views here.
+def show_katalog(request):
+    item_list_catalog = CatalogItem.objects.all()
+    context = {
+    'item_list': item_list_catalog,
+    'nama': 'Hanif'
+    }
+    return render(request, "katalog.html", context)
