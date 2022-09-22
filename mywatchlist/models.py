82b9661533b1b742fa54da1c0703fdeb11175cbc
Ikhlasul Akmal Hanif <2112akmal@gmail.com>
2022-09-22 10:38:33 +0700

    edit logic buat bonus

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class MyWatchlistItem(models.Model):
-    watched = models.TextField()
+    watched = models.BooleanField()
     title = models.TextField()
     rating = models.IntegerField()
     release_date = models.TextField()
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,19 @@ from django.core import serializers
 
 def show_mywatchlist(request):
     item_list_watchlist = MyWatchlistItem.objects.all()
+    watched_cnt = 0
+    for item in item_list_watchlist:
+        if item.watched:
+            watched_cnt += 1
+    if (watched_cnt*2 >= len(item_list_watchlist)):
+        is_watched_cnt = True
+    else:
+        is_watched_cnt = False
     context = {
     'item_list': item_list_watchlist,
     'nama': 'Ikhlasul Akmal Hanif',
     'npm' : '2106650462',
+    'is_watched_cnt' : is_watched_cnt
     }
     return render(request, "mywatchlist.html", context)
 
